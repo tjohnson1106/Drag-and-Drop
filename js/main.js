c8237392c40daf2102cab0e0f87dd51ff432defb
Thomas Johnson <tjohnson1106@gmail.com>
2018-05-30 17:50:20 -0400

    created empty and fill event listeners

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
+
+//fill listeners
+
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
+
+//loop through empties call events
+
+for (const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
+
+//drag functions
+
+function dragStart() {
+  this.className += "hold";
+  setTimeout(() => (this.className = "invisble"), 0);
+}
+
+function dragEnd() {
+  this.className = "fill";
+}
